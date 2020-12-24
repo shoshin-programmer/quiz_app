44b2a80dd8d89f2c1f5c20072dd9fb2815137b1a
shoshin-programmer <atienza.new@gmail.com>
2020-12-24 15:32:23 +0800

    inital css configurations

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
@@ -7,6 +7,14 @@ export default function Home() {
         <title>Sample Quiz App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className="hero fullscreen main-bg">
+        <div className="hero-body">
+          <div className="mx-auto">
+            <h1 className="title uppercase text-gray-100">Look!</h1>
+            <h3 className="subtitle text-gray-500">I am tall!</h3>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
